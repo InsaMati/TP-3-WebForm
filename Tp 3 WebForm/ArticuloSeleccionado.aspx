edc0670b6449b06f145ceddd78a0659fa5d631c4
Matias <matiasinsaurralde51@gmail.com>
2020-10-12 11:08:07 -0300

    Error corregido en ArticuloSeleccionado.aspx

--- Tp 3 WebForm/ArticuloSeleccionado.aspx
+++ Tp 3 WebForm/ArticuloSeleccionado.aspx
@@ -1,3 +1,25 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ArticuloSeleccionado.aspx.cs" Inherits="Tp_3_WebForm.ArticuloSeleccionado" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <br />
+
+    <div class="card">
+        <img src="<% = MostrarArticulo.UrlImagen %>" class="card-img-top" width="250" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><% = MostrarArticulo.Nombre %></h5>
+            <p title="Descripcion:" class="card-text"><% = MostrarArticulo.Descripcion %></p>
+            <p title="Precio: $" class="card-text"><%=MostrarArticulo.Precio%></p>
+
+        </div>
+        <asp:Label Text="Cantidad" runat="server"></asp:Label>
+        <asp:TextBox ID="TxtCantidad" Height="25px" Width="25px" runat="server"></asp:TextBox>
+        <asp:Button ID="BtnDisminuir" CssClass="btn btn-primary" runat="server" Text="-" Height="30px" Width="33px" OnClick="BtnDisminuir_Click" />
+        <asp:Button ID="BtnAumentar" CssClass="btn btn-primary" runat="server" Text="+" Height="30px" Width="33px" OnClick="BtnAumentar_Click" />
+    </div>
+
+
+
+
+
 </asp:Content>
--- Tp 3 WebForm/ArticuloSeleccionado.aspx.cs
+++ Tp 3 WebForm/ArticuloSeleccionado.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +11,29 @@ namespace Tp_3_WebForm
 {
     public partial class ArticuloSeleccionado : System.Web.UI.Page
     {
+        public Articulo MostrarArticulo { get; set; }
+        public List<Articulo> ListaArticulos { get; set; }
+
+        public List<Carrito> ListaCarro = new List<Carrito>();
+
+        public Carrito Carro = new Carrito();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            NegocioArticulo NegocioArticulo = new NegocioArticulo();
+
+            if (!IsPostBack)
+            {
+                //////
+            }
+
             try
             {
+                ListaArticulos = NegocioArticulo.ListaDeArticulos();
+
+                var ArtSelec = Convert.ToInt32(Request.QueryString["Id"]);
+
+                MostrarArticulo = ListaArticulos.Find(J => J.Id == ArtSelec);
 
             }
             catch (Exception ex)
@@ -20,6 +41,16 @@ namespace Tp_3_WebForm
 
                 throw ex;
             }
+        }
+
+        protected void BtnDisminuir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnAumentar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
